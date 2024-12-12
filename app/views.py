290e3e654ab7668193b0a59a4fe65013e15f9c0b
silvyashree <silvyashree.com>
2024-12-12 12:17:02 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def boots(request):
+    return render (request,'boots.html')
+def wrok1(request):
+    return render (request,'wrok1.html')
